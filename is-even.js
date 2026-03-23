bff4df93ef5f26b704f86c9981918d354f7bd693
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2026-03-23 14:13:19 +0100

    added is-even.jd file

---
+++ is-even.js
@@ -0,0 +1,10 @@
+function isEven(num) {
+  if (num % 2 === 0) {
+    console.log(`isEven(${num}) → ${true}`);
+  } else {
+    console.log(`isEven(${num}) → ${false}`);
+  }
+}
+
+isEven(4);
+isEven(7);
